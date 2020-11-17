f38ff5dfbea24d4141345989bd5805ec975cea89
Linux Kyrios <malinowskim92@protonmail.com>
2020-11-17 19:40:28 +0100

    Initial commit

---
+++ app/src/main/java/com/linuxkyrios/tuxconstraintlayout/MainActivity.java
@@ -0,0 +1,14 @@
+package com.linuxkyrios.tuxconstraintlayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
